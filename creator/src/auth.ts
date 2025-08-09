567afa6353dde988947fa15e8f689b1ebbee4197
Sota <sota.matsui@akane.waseda.jp>
2025-08-09 21:05:37 +0900

    debug

--- creator/src/auth.ts
+++ creator/src/auth.ts
@@ -3,6 +3,21 @@ import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
+    signIn({ user, account, profile }) {
+      console.log("signIn callback", user, account, profile);
+      return true;
+    },
+    redirect({ url, baseUrl }) {
+      console.log("redirect callback", url, baseUrl);
+      return baseUrl;
+    },
+    jwt({ token, user }) {
+      console.log("jwt callback", token, user);
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
     session({ session, user }) {
       console.log("session callback", session, user);
       if (session.user) {
