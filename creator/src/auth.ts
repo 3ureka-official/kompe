ebc0db0f53d7868bf46e93b94e9afaaf882ff20b
Sota <sota.matsui@akane.waseda.jp>
2025-08-09 19:36:20 +0900

    readiing env

--- creator/src/auth.ts
+++ creator/src/auth.ts
@@ -4,6 +4,8 @@ import TikTok from "next-auth/providers/tiktok";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     TikTok({
+      clientId: process.env.AUTH_TIKTOK_ID,
+      clientSecret: process.env.AUTH_TIKTOK_SECRET,
       userinfo:
         "https://open.tiktokapis.com/v2/user/info/?fields=open_id,union_id,avatar_url,display_name,username",
       profile(profile) {
