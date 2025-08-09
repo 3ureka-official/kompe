4092189c5c6f6843d724eeadc38f9b580eaa7c85
Sota <sota.matsui@akane.waseda.jp>
2025-08-09 20:30:59 +0900

    testing callbacks

--- creator/src/auth.ts
+++ creator/src/auth.ts
@@ -2,6 +2,15 @@ import NextAuth from "next-auth";
 import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    session({ session, user }) {
+      console.log("session callback", session, user);
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   providers: [
     TikTok({
       clientId: process.env.AUTH_TIKTOK_ID,
