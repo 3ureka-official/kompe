e890866247b342d8b78574dc60fbed109b80684e
Sota <sota.matsui@akane.waseda.jp>
2025-08-09 19:20:47 +0900

    testing custom provider

--- creator/src/auth.ts
+++ creator/src/auth.ts
@@ -2,5 +2,19 @@ import NextAuth from "next-auth";
 import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [TikTok],
+  providers: [
+    TikTok({
+      userinfo:
+        "https://open.tiktokapis.com/v2/user/info/?fields=open_id,union_id,avatar_url,display_name,username",
+      profile(profile) {
+        console.log("TikTok profile", profile);
+        return {
+          id: profile.data.user.open_id || profile.data.user.union_id,
+          name: profile.data.user.display_name,
+          image: profile.data.user.avatar_url,
+          email: profile.data.user.email || profile.data.user.username || null,
+        };
+      },
+    }),
+  ],
 });
