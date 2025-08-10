704c8535737faf9537bf8f9bfe25a81b7c0d8186
Sota <sota.matsui@akane.waseda.jp>
2025-08-11 02:59:18 +0900

    feat: implement dashboard page

--- creator/src/components/sign-in.tsx
+++ creator/src/components/sign-in.tsx
@@ -1,38 +1,24 @@
-import { auth, signIn } from "@/auth";
+import { signIn } from "@/auth";
 import Image from "next/image";
 
 export default async function SignIn() {
-  const session = await auth();
-  if (!session)
-    return (
-      <form
-        action={async () => {
-          "use server";
-          await signIn("tiktok");
-        }}
-      >
-        <button className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-background gap-2 hover:text-gray-200 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
-          <Image
-            className="dark:invert"
-            src="/external/TikTok_Icon_Black_Square.png"
-            alt="TikTok logomark"
-            width={28}
-            height={28}
-          />
-          TikTokアカウントで始める
-        </button>
-      </form>
-    );
   return (
-    <div>
-      <ul>
-        <li>email {session.user?.email}</li>
-        <li>name {session.user?.name}</li>
-        <li>image {session.user?.image}</li>
-        <li>id {session.user?.id}</li>
-        <li>expires {session.expires}</li>
-      </ul>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-    </div>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("tiktok", { redirectTo: "/dashboard" });
+      }}
+    >
+      <button className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-background gap-2 hover:text-gray-200 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
+        <Image
+          className="dark:invert"
+          src="/external/TikTok_Icon_Black_Square.png"
+          alt="TikTok logomark"
+          width={28}
+          height={28}
+        />
+        TikTokアカウントで始める
+      </button>
+    </form>
   );
 }
