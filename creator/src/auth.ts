c87688c46ac8a8db0f90202bae2405473051a849
Sota <sota.matsui@akane.waseda.jp>
2025-08-10 15:10:16 +0900

    chore: remove comments

--- creator/src/auth.ts
+++ creator/src/auth.ts
@@ -2,18 +2,14 @@ import NextAuth from "next-auth";
 import TikTok from "next-auth/providers/tiktok";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
   callbacks: {
     jwt({ token, user }) {
-      console.log("JWT Callback", { token, user });
       if (user) {
-        // User is available during sign-in
         token.id = user.id;
       }
       return token;
     },
     session({ session, token }) {
-      console.log("Session Callback", { session, token });
       session.user.id = token.id as string;
       return session;
     },
