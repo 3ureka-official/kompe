176001784edfff015e37036de22ffa374d60151b
sota <sota.matsui@akane.waseda.jp>
2025-08-17 11:26:26 +0900

    feat: implement `competitions` and `competitions/{id}` page

---
+++ creator/src/app/competitions/page.tsx
@@ -0,0 +1,64 @@
+import prisma from "@/lib/prisma";
+import { formatDistanceToNow } from "date-fns";
+import { ja } from "date-fns/locale";
+import Link from "next/link";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import { ArrowRightIcon, BanknoteIcon, CalendarClockIcon } from "lucide-react";
+
+export default async function Competitions() {
+  const data = await prisma.contests.findMany({ include: { brands: true } });
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold px-2 py-8">開催中のコンペティション</h1>
+      <div>
+        {data.map((competition) => (
+          <Card key={competition.id}>
+            <CardHeader>
+              <div className="flex items-center gap-4">
+                <Avatar>
+                  <AvatarImage src={competition.brands.logo_url || '' /* todo: fallback image */} />
+                  <AvatarFallback className="uppercase">{competition.brands.name.split('', 2)}</AvatarFallback>
+                </Avatar>
+                <p>{competition.brands.name}</p>
+              </div>
+              <CardAction className="h-full flex items-center gap-2 font-bold text-xl">
+                <BanknoteIcon className="size-6 stroke-2" />¥{competition.prize_pool?.toLocaleString()}
+              </CardAction>
+            </CardHeader>
+            <CardContent className="grid gap-2">
+              <CardTitle className="text-lg font-bold">{competition.title}</CardTitle>
+              <Image
+                src={competition.thumbnail_url || ''/* todo: add fallback image */}
+                alt={competition.title || 'タイトル未設定のコンペティション'}
+                width={500}
+                height={300}
+                className="rounded-lg"
+              />
+            </CardContent>
+            <CardFooter className="justify-between">
+              <div className="flex items-center gap-2 text-muted-foreground font-bold">
+                <CalendarClockIcon />
+                <p>{`残り${formatDistanceToNow(new Date(competition.contest_end_date), { locale: ja })}`}</p>
+              </div>
+              <Button asChild>
+                <Link href={`/competitions/${competition.id}`}>
+                  <ArrowRightIcon />今すぐ参加
+                </Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
--- creator/src/app/layout.tsx
+++ creator/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="flex items-center gap-4 px-4 py-8">
+          <Image
+            src="/logo-colored.svg"
+            alt="Kompe logo"
+            width={140}
+            height={38}
+            priority
+          />
+        </nav>
         {children}
       </body>
     </html>
