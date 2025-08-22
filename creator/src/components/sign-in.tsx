847c37165afea814cf4b247c207099296bc50af6
sota <sota.matsui@akane.waseda.jp>
2025-08-22 17:43:09 +0900

    feat: remove unused components and implement new competition pages

--- creator/src/components/sign-in.tsx
+++ creator/src/components/sign-in.tsx
@@ -1,7 +1,17 @@
 import { signIn } from "@/auth";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
-export default async function SignIn() {
+type Props = {
+  variant?: "default" | "minimal";
+}
+
+const buttonTexts = {
+  default: "TikTokアカウントで始める",
+  minimal: "ログイン"
+}
+
+export default async function SignIn({ variant = 'default' }: Props) {
   return (
     <form
       action={async () => {
@@ -9,7 +19,7 @@ export default async function SignIn() {
         await signIn("tiktok", { redirectTo: "/dashboard" });
       }}
     >
-      <button className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-background gap-2 hover:text-gray-200 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
+      <Button className="bg-black">
         <Image
           className="dark:invert"
           src="/external/TikTok_Icon_Black_Square.png"
@@ -17,8 +27,8 @@ export default async function SignIn() {
           width={28}
           height={28}
         />
-        TikTokアカウントで始める
-      </button>
+        {buttonTexts[variant]}
+      </Button>
     </form>
   );
 }
