99c64b86dd61d0f129e468cd8f83c82b86342e19
Sota <sota.matsui@akane.waseda.jp>
2025-09-08 00:50:59 +0900

    feat: implement dashboard page

--- creator/src/app/(authenticated)/dashboard/page.tsx
+++ creator/src/app/(authenticated)/dashboard/page.tsx
@@ -1,30 +1,148 @@
 import { auth } from "@/auth";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { tikTokAPIClient } from "@/lib/api/tiktok";
+import prisma from "@/lib/prisma";
+import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
+import { ja } from "date-fns/locale";
+import {
+  BadgeCheckIcon,
+  BanknoteIcon,
+  CalendarClockIcon,
+  ChevronRightIcon,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Dashboard() {
   const session = await auth();
-  const userInfo = await tikTokAPIClient.getUserInfo(['display_name', 'username', 'avatar_large_url', 'follower_count', 'following_count', 'video_count', 'likes_count']);
   if (!session) {
     return <div>ログインしてください。</div>;
   }
+  const userInfo = await tikTokAPIClient.getUserInfo([
+    "display_name",
+    "username",
+    "avatar_url_100",
+    "follower_count",
+    "following_count",
+    "video_count",
+    "likes_count",
+  ]);
   if (!userInfo) {
     return <div>ユーザー情報の取得に失敗しました。</div>;
   }
-  const user = userInfo.data.user;
+  const applications = await prisma.applications.findMany({
+    where: { creator_id: session.user.creator_id },
+    include: { contests: { include: { brands: true } } },
+  });
+  const tiktokUser = userInfo.data.user;
   return (
     <div className="w-full grid gap-8 p-8">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <Card className="bg-black text-white">
+        <CardHeader>
+          <CardTitle className="text-sm font-bold">
+            TikTokプロフィール
+          </CardTitle>
+          <CardDescription className="text-gray-500 font-semibold">
+            TikTokアカウントと連携しています
+          </CardDescription>
+          <CardAction>
+            <Badge className="bg-pink-600">
+              連携済み
+              <BadgeCheckIcon />
+            </Badge>
+          </CardAction>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center">
+          <Image
+            src={tiktokUser.avatar_url_100 || ""}
+            alt="Avatar"
+            width={96}
+            height={96}
+            className="rounded-full m-4 size-24"
+          />
+          <p className="text-center text-lg font-bold">
+            {tiktokUser.display_name}
+          </p>
+          <p className="text-center text-sm text-gray-500 font-semibold">
+            @{tiktokUser.username}
+          </p>
+        </CardContent>
+        <CardFooter className="grid grid-cols-3 text-center">
+          <div>
+            <p className="font-bold">{tiktokUser.following_count}</p>
+            <p className="text-sm text-gray-500 font-semibold">フォロー中</p>
+          </div>
+          <div>
+            <p className="font-bold">{tiktokUser.follower_count}</p>
+            <p className="text-sm text-gray-500 font-semibold">フォロワー</p>
+          </div>
+          <div>
+            <p className="font-bold">{tiktokUser.likes_count}</p>
+            <p className="text-sm text-gray-500 font-semibold">いいね</p>
+          </div>
+        </CardFooter>
+      </Card>
       <Card>
         <CardHeader>
-          <CardTitle>{user.display_name}</CardTitle>
-          <CardDescription>@{user.username}</CardDescription>
+          <CardTitle>コンペティション</CardTitle>
+          <CardDescription>Kompeで参加したコンペティション</CardDescription>
+          <CardAction className="text-3xl font-bold">
+            {applications.length}
+          </CardAction>
         </CardHeader>
         <CardContent>
-          <p>Followers: {user.follower_count}</p>
-          <p>Following: {user.following_count}</p>
-          <p>Videos: {user.video_count}</p>
-          <p>Likes: {user.likes_count}</p>
+          {applications.length === 0 && (
+            <p className="text-center text-muted-foreground text-sm">
+              まだコンペティションに参加していません。
+            </p>
+          )}
+          {applications.length > 0 && (
+            <div className="grid gap-4">
+              {applications.map((app) => {
+                const competition = app.contests;
+                return (
+                  <Link href={`/competitions/${competition.id}`}>
+                    <Card className="h-[100px] py-4">
+                      <CardContent className="h-full px-4">
+                        <div className="h-full flex items-center gap-4">
+                          <Image
+                            src={
+                              competition.thumbnail_url ||
+                              "" /* todo: add fallback image */
+                            }
+                            alt={
+                              competition.title ||
+                              "タイトル未設定のコンペティション"
+                            }
+                            width={500}
+                            height={300}
+                            className="h-full w-auto rounded-lg"
+                          />
+                          <div>
+                            <p>{competition.title}</p>
+                            <p className="text-xs text-muted-foreground">
+                              {competition.brands.name}
+                            </p>
+                          </div>
+                          <ChevronRightIcon className="size-4 stroke-2 ml-auto" />
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
         </CardContent>
       </Card>
     </div>
