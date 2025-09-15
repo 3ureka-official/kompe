ab9fe4e063c7c9f99947a90bc89cce278d0ca209
Sota <sota.matsui@akane.waseda.jp>
2025-09-15 16:10:32 +0900

    fix: using original middleware instead to avoid accessing prisma in edge env

--- creator/src/middleware.ts
+++ creator/src/middleware.ts
@@ -1,4 +1,11 @@
-export { auth as middleware } from "@/auth";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = request.cookies;
+  const session = await cookieStore.get("__Secure-authjs.session-token");
+  if (!session)
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+}
 export const config = {
   matcher: [
     "/((?!api|competitions|login|_next/static|_next/image|favicon.ico|.*\\.svg|$).*)",
