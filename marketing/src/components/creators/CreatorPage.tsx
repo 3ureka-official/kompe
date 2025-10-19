94e0f377881077f9f705066e487b584256d2d14d
kyatomaru <souryu1010@gmail.com>
2025-10-19 17:58:04 +0900

    fix: Heroのズレを解消

--- marketing/src/components/creators/CreatorPage.tsx
+++ marketing/src/components/creators/CreatorPage.tsx
@@ -5,10 +5,23 @@ import { CreatorHowItWorks } from "@/components/creators/CreatorHowItWorks";
 import { CreatorFAQ } from "@/components/creators/CreatorFAQ";
 import { CreatorFinalCTA } from "@/components/creators/CreatorFinalCTA";
 import { CreatorTopCTA } from "@/components/creators/CreatorTopCTA";
+import Image from "next/image";
 
 export default function CreatorsPage() {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
+      <span
+        style={{ position: "fixed", top: "2.5vw", left: "4.5vw", zIndex: 200 }}
+      >
+        <Image
+          src="/images/logo/logo-coloredhdpi.png"
+          alt="Creator Hero"
+          width={0}
+          height={0}
+          sizes="100%"
+          className="w-[25vw] xl:w-[160px]"
+        />
+      </span>
       <CreatorHero />
       <CreatorTopCTA />
       <CreatorProblemSection />
