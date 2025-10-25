90d09087812e298bfc41bfb2ff1185bb2fd3a61b
kyatomaru <souryu1010@gmail.com>
2025-10-26 01:19:37 +0900

    feat: 賞金テーブルとリーダーボードのUIを修正

--- creator/src/components/leaderBoard.tsx
+++ creator/src/components/leaderBoard.tsx
@@ -1,12 +1,6 @@
 import { formatJpy } from "@/utils/format";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "./ui/table";
+import { ExternalLinkIcon, PlayIcon } from "lucide-react";
+import { Table, TableBody, TableCell, TableRow } from "./ui/table";
 import {
   contests,
   applications,
@@ -14,6 +8,9 @@ import {
   contest_transfers,
 } from "@prisma/client";
 import { formatDate } from "@/utils/format";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+import { auth } from "@/auth";
 
 type LeaderBoardType = {
   competition: contests;
@@ -28,34 +25,67 @@ export default async function LeaderBoard({
   competition,
   applications,
 }: LeaderBoardType) {
+  const session = await auth();
+
   return (
     <div>
-      <div className="text-sm text-muted-foreground mb-2">
+      <div className="text-sm text-muted-foreground mb-2 px-4">
         前回の更新: {formatDate(competition.updated_engagement_at)}
       </div>
       <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>順位</TableHead>
-            <TableHead>クリエイター</TableHead>
-            <TableHead className="text-right">再生数</TableHead>
-            <TableHead className="text-right">賞金</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {applications && applications.length > 0 ? (
-            applications?.map((application, index) => (
-              <TableRow key={application.id}>
-                <TableCell className="font-medium">#{index + 1}</TableCell>
-                <TableCell>{application.creators.display_name}</TableCell>
-                <TableCell className="text-right">
-                  {application.views.toLocaleString()}
-                </TableCell>
-                <TableCell className="text-right">
-                  {formatJpy(competition.prize_distribution[index])}
-                </TableCell>
-              </TableRow>
-            ))
+        <TableBody className="border-y border-y-gray">
+          {applications &&
+          applications.filter((app) => app.tiktok_url !== null).length > 0 ? (
+            applications
+              .filter((app) => app.tiktok_url !== null)
+              .map((application, index) => (
+                <TableRow key={application.id} className="h-12">
+                  <TableCell className="font-medium w-6 px-2">
+                    <p
+                      className={`text-sm font-bold h-6 w-6 rounded-full flex items-center justify-center ${session?.user?.creator_id === application.creator_id ? "bg-primary text-primary-foreground" : ""}`}
+                    >
+                      {index + 1}
+                    </p>
+                  </TableCell>
+                  <TableCell className="w-12">
+                    <Avatar className="size-12">
+                      <AvatarImage
+                        src={
+                          application.creators.avatar_url ||
+                          "" /* todo: fallback image */
+                        }
+                      />
+                      <AvatarFallback className="uppercase">
+                        {application.creators.display_name?.split("", 2)}
+                      </AvatarFallback>
+                    </Avatar>
+                  </TableCell>
+                  <TableCell className="flex flex-col gap-1 text-left w-full">
+                    <p className="text-sm">
+                      {application.creators.display_name}
+                    </p>
+                    <span className="flex items-center gap-1">
+                      <PlayIcon className="size-4 stroke-2" />
+                      <p className="text-sm">
+                        {application.views.toLocaleString()}
+                      </p>
+                    </span>
+                  </TableCell>
+                  <TableCell className="w-12">
+                    <p className="text-base font-bold">
+                      {formatJpy(competition.prize_distribution[index])}
+                    </p>
+                  </TableCell>
+                  <TableCell className="text-right w-5 pr-4">
+                    <Link
+                      href={`https://www.tiktok.com/@${application.creators.username}/video/${application.tiktok_url?.split("/").pop()}`}
+                      target="_blank"
+                    >
+                      <ExternalLinkIcon className="size-4 stroke-2" />
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))
           ) : (
             <TableRow>
               <TableCell colSpan={4} className="text-center">
