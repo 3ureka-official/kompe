58fdf38ceb47abfc3c2ff0cf829c7d923e0b491d
kyatomaru <souryu1010@gmail.com>
2025-10-25 12:12:09 +0900

    feat(home): コンテストがない場合の表示

--- creator/src/app/(home)/page.tsx
+++ creator/src/app/(home)/page.tsx
@@ -31,59 +31,67 @@ export default async function Competitions() {
     <div className="p-4 bg-gray-50">
       <h1 className="text-lg font-bold pb-4">開催中のコンテスト</h1>
       <div className="grid gap-4">
-        {data.map((competition) => (
-          <Link href={`/competitions/${competition.id}`} key={competition.id}>
-            <Card className="py-3 gap-2">
-              <CardHeader className="px-3">
-                <div className="flex items-center gap-4">
-                  <Avatar>
-                    <AvatarImage
-                      src={
-                        competition.brands.logo_url ||
-                        "" /* todo: fallback image */
-                      }
-                    />
-                    <AvatarFallback className="uppercase">
-                      {competition.brands.name.split("", 2)}
-                    </AvatarFallback>
-                  </Avatar>
-                  <p>{competition.brands.name}</p>
-                </div>
-              </CardHeader>
-              <CardContent className="grid gap-2 px-3">
-                <Image
-                  src={
-                    competition.thumbnail_url ||
-                    "" /* todo: add fallback image */
-                  }
-                  alt={competition.title || "タイトル未設定のコンペティション"}
-                  width={500}
-                  height={300}
-                  className="rounded-lg"
-                />
-                <CardTitle className="text-lg font-bold my-2 h-[3em] overflow-hidden text-ellipsis line-clamp-2">
-                  {competition.title}
-                </CardTitle>
-              </CardContent>
-              <CardFooter className="justify-between px-3 py-2">
-                <div className="flex items-center justify-between gap-4">
-                  <div className="flex items-center gap-1 text-muted-foreground font-bold">
-                    <VideoIcon className="size-5 stroke-2" />
-                    <p>{competition.applications.length}</p>
+        {data.length > 0 ? (
+          data.map((competition) => (
+            <Link href={`/competitions/${competition.id}`} key={competition.id}>
+              <Card className="py-3 gap-2">
+                <CardHeader className="px-3">
+                  <div className="flex items-center gap-4">
+                    <Avatar>
+                      <AvatarImage
+                        src={
+                          competition.brands.logo_url ||
+                          "" /* todo: fallback image */
+                        }
+                      />
+                      <AvatarFallback className="uppercase">
+                        {competition.brands.name.split("", 2)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <p>{competition.brands.name}</p>
                   </div>
-                  <div className="flex items-center gap-1 text-muted-foreground font-bold">
-                    <ClockIcon className="size-5 stroke-2" />
-                    <p>{`残り${formatDistanceToNow(new Date(competition.contest_end_date), { locale: ja })}`}</p>
+                </CardHeader>
+                <CardContent className="grid gap-2 px-3">
+                  <Image
+                    src={
+                      competition.thumbnail_url ||
+                      "" /* todo: add fallback image */
+                    }
+                    alt={
+                      competition.title || "タイトル未設定のコンペティション"
+                    }
+                    width={500}
+                    height={300}
+                    className="rounded-lg"
+                  />
+                  <CardTitle className="text-lg font-bold my-2 h-[3em] overflow-hidden text-ellipsis line-clamp-2">
+                    {competition.title}
+                  </CardTitle>
+                </CardContent>
+                <CardFooter className="justify-between px-3 py-2">
+                  <div className="flex items-center justify-between gap-4">
+                    <div className="flex items-center gap-1 text-muted-foreground font-bold">
+                      <VideoIcon className="size-5 stroke-2" />
+                      <p>{competition.applications.length}</p>
+                    </div>
+                    <div className="flex items-center gap-1 text-muted-foreground font-bold">
+                      <ClockIcon className="size-5 stroke-2" />
+                      <p>{`残り${formatDistanceToNow(new Date(competition.contest_end_date), { locale: ja })}`}</p>
+                    </div>
                   </div>
-                </div>
-                <CardAction className="h-full flex items-center gap-2 font-bold text-xl text-primary">
-                  <CrownIcon className="size-5 stroke-2 text-muted-foreground" />
-                  ¥{competition.prize_pool?.toLocaleString()}
-                </CardAction>
-              </CardFooter>
-            </Card>
-          </Link>
-        ))}
+                  <CardAction className="h-full flex items-center gap-2 font-bold text-xl text-primary">
+                    <CrownIcon className="size-5 stroke-2 text-muted-foreground" />
+                    ¥{competition.prize_pool?.toLocaleString()}
+                  </CardAction>
+                </CardFooter>
+              </Card>
+            </Link>
+          ))
+        ) : (
+          <div className="text-sm text-muted-foreground">
+            開催中のコンテストがありません
+          </div>
+        )}
       </div>
     </div>
   );
