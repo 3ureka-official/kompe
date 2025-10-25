f2f41e60524f37f5d65bb517555ba1ef93782882
kyatomaru <souryu1010@gmail.com>
2025-10-26 01:19:09 +0900

    feat: 動画未選択時の表示を追加

--- creator/src/components/buttomActionBar.tsx
+++ creator/src/components/buttomActionBar.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon } from "lucide-react";
+import { CheckIcon, CircleAlertIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
@@ -27,6 +27,15 @@ const getIsApplied = async (applications: applications[]) => {
   const session = await auth();
   if (!session) return null;
   return applications.find(
+    (app) =>
+      app.creator_id === session.user?.creator_id && app.tiktok_url !== null,
+  );
+};
+
+const getIsConcent = async (applications: applications[]) => {
+  const session = await auth();
+  if (!session) return null;
+  return applications.find(
     (app) => app.creator_id === session.user?.creator_id,
   );
 };
@@ -47,44 +56,56 @@ export default async function ButtomActionBar({
   const session = await auth();
 
   const isApplied = await getIsApplied(applications || []);
+  const isConcent = await getIsConcent(applications || []);
   const ranking = await getRanking(applications || []);
 
   return (
     <div className="absolute bottom-0 left-0 right-0 bg-card border border-b-0 rounded-t-2xl w-full p-4">
       {session ? (
-        isApplied ? (
-          applications && isEnded && ranking !== null ? (
-            applications?.[ranking]?.contest_transfers?.stripe_transfer_id ? (
-              <p className="text-sm text-muted-foreground">
-                このコンテストは終了しました。
-                <br />
-                順位：{ranking + 1}位
-              </p>
-            ) : (
-              <GetPrizeDialog
-                competition={competition}
-                application={applications[ranking]}
-                contestTransfers={applications[ranking]?.contest_transfers}
-                ranking={ranking}
-              />
-            )
+        applications && isEnded && ranking !== null ? (
+          applications?.[ranking]?.contest_transfers?.stripe_transfer_id ? (
+            <p className="text-sm text-muted-foreground">
+              このコンテストは終了しました。
+              <br />
+              順位：{ranking + 1}位
+            </p>
           ) : (
-            <Button className="w-full py-5" asChild>
-              <Link href={`/applications/${competition.id}`}>
-                <Badge variant={"secondary"}>
-                  <CheckIcon />
-                  応募済み
-                </Badge>
-                参加動画の管理
-              </Link>
-            </Button>
+            <GetPrizeDialog
+              competition={competition}
+              application={applications[ranking]}
+              contestTransfers={applications[ranking]?.contest_transfers}
+              ranking={ranking}
+            />
           )
+        ) : isApplied ? (
+          <Button className="w-full py-5 font-bold" asChild>
+            <Link href={`/applications/${competition.id}`}>
+              <Badge variant={"secondary"} className="font-bold">
+                <CheckIcon />
+                応募済み
+              </Badge>
+              参加動画の管理
+            </Link>
+          </Button>
+        ) : isConcent ? (
+          <Button className="w-full py-5 font-bold" asChild>
+            <Link href={`/applications/${competition.id}`}>
+              <Badge
+                variant={"secondary"}
+                className="bg-white text-destructive font-bold"
+              >
+                <CircleAlertIcon />
+                動画未選択
+              </Badge>
+              参加動画の管理
+            </Link>
+          </Button>
         ) : (
           <ApplyDialog competitionId={competition.id} />
         )
       ) : (
         <SignInButton
-          className="*:w-full py-5"
+          className="*:w-full *:py-5"
           redirectTo={`/competitions/${competition.id}`}
         />
       )}
