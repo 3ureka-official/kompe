9b2f91d96dddd607728cb0bec5e62fcf9afbbdfd
kyatomaru <souryu1010@gmail.com>
2025-10-26 16:53:39 +0900

    feat(footer): 参加履歴タブを追加

--- creator/src/components/bottomNavigationBar.tsx
+++ creator/src/components/bottomNavigationBar.tsx
@@ -1,20 +1,28 @@
-import { CrownIcon, UserIcon } from "lucide-react";
+import { CrownIcon, BriefcaseBusinessIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 
 type Props = {
-  currentPage: "competitions" | "mypage" | "other";
+  currentPage: "competitions" | "work-histories" | "mypage" | "other";
 };
 
 export default async function BottomNavigationBar({ currentPage }: Props) {
   return (
-    <nav className="bg-card border rounded-t-2xl w-full p-4">
-      <div className="grid grid-cols-2 text-center">
+    <nav className="fixed bottom-0 left-0 right-0 bg-card border rounded-t-2xl w-full py-4">
+      <div className="grid grid-cols-3 text-center mx-auto max-w-md w-full">
         <Link href="/">
           <div className="flex flex-col items-center text-primary">
             <CrownIcon
               className={`size-4 stroke-2 ${currentPage === "competitions" && "fill-primary"}`}
             />
             <p className="text-xs font-semibold">コンテスト</p>
+          </div>
+        </Link>
+        <Link href="/work-histories">
+          <div className="flex flex-col items-center text-primary relative">
+            <BriefcaseBusinessIcon
+              className={`size-4 stroke-2 ${currentPage === "work-histories" && "fill-primary"}`}
+            />
+            <p className="text-xs font-semibold">参加履歴</p>
           </div>
         </Link>
         <Link href="/mypage">
