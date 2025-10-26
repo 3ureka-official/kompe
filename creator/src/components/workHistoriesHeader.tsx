fac9db01135ca621cdcce9d29625112f00cd7079
kyatomaru <souryu1010@gmail.com>
2025-10-26 16:45:09 +0900

    feat(applied-history): 参加履歴のページを実装

---
+++ creator/src/components/workHistoriesHeader.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Session } from "next-auth";
+import HeaderAvatar from "./headerAvatar";
+import SignInButton from "./signInButton";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export const WorkHistoriesHeader = ({
+  session,
+}: {
+  session: Session | null;
+}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleTabChange = (value: string) => {
+    router.push(`/work-histories?tab=${value}`);
+  };
+
+  const activeTab =
+    (searchParams.get("tab") as "ongoing" | "ended") || "ongoing";
+
+  return (
+    <nav className="w-full flex flex-col gap-4 px-6 pt-4 border-b">
+      <div className="flex items-center justify-between gap-4">
+        <Link href="/">
+          <Image
+            src="/logo-colored.svg"
+            alt="Kompe logo"
+            width={120}
+            height={38}
+            priority
+          />
+        </Link>
+        {session ? (
+          <HeaderAvatar session={session} />
+        ) : (
+          <SignInButton variant="minimal" />
+        )}
+      </div>
+      <Tabs value={activeTab} onValueChange={handleTabChange}>
+        <TabsList className="w-full">
+          <TabsTrigger value="ongoing">開催中</TabsTrigger>
+          <TabsTrigger value="ended">終了</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </nav>
+  );
+};
