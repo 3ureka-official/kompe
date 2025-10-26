6e5585cff6eb8053570c55ddec1b70471eebccdd
kyatomaru <souryu1010@gmail.com>
2025-10-26 12:14:56 +0900

    feat(my-page): ヘッダー追加

--- creator/src/app/(authenticated)/mypage/layout.tsx
+++ creator/src/app/(authenticated)/mypage/layout.tsx
@@ -1,12 +1,16 @@
 import BottomNavigationBar from "@/components/bottomNavigationBar";
+import { MainHeader } from "@/components/mainHeader";
+import { auth } from "@/auth";
 
 export default async function MyPageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <>
+      <MainHeader session={session} />
       <main className="grow min-h-0 overflow-auto relative">{children}</main>
       <BottomNavigationBar currentPage="mypage" />
     </>
