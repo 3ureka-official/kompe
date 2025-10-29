d43a4a995ef5dd7cc1ce384663213104353b3776
kyatomaru <souryu1010@gmail.com>
2025-10-29 20:22:33 +0900

    feat: 余白の修正

--- creator/src/app/(home)/page.tsx
+++ creator/src/app/(home)/page.tsx
@@ -12,7 +12,7 @@ export default async function HomePage({ searchParams }: HomePageProps) {
   const period = (await searchParams)?.period ?? "all";
 
   return (
-    <div className="p-4 min-h-full pb-20">
+    <div className="p-4 min-h-full pb-30">
       <div className="flex items-center justify-between pb-4">
         <h1 className="text-lg font-bold ">コンテスト一覧</h1>
         <ContestFilterSelect />
