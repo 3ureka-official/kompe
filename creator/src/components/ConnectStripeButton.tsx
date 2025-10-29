367e6cf3c7357ea8dd6d5ba25da3ef2c6faa32db
kyatomaru <souryu1010@gmail.com>
2025-10-29 14:03:34 +0900

    feat: Stripeの外部リンク移動時のローディング追加

--- creator/src/components/ConnectStripeButton.tsx
+++ creator/src/components/ConnectStripeButton.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import Loading from "./loading";
 
 export default function ConnectStripeButton() {
+  const [open, setOpen] = useState(false);
   const connectToStripe = async () => {
+    setOpen(true);
     try {
       const res = await fetch("/api/stripe/connect/account/create", {
         method: "POST",
@@ -11,11 +15,24 @@ export default function ConnectStripeButton() {
       });
 
       const data = await res.json();
-      window.location.assign(data.url);
+      window.open(data.url, "_blank", "noopener,noreferrer");
     } catch (err) {
       console.error(err);
+    } finally {
+      setOpen(false);
     }
   };
 
-  return <Button onClick={connectToStripe}>入金口座を登録</Button>;
+  return (
+    <>
+      <Button onClick={connectToStripe} className="py-5 font-bold">
+        入金口座を登録
+      </Button>
+      {open && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-700/50 z-50">
+          <Loading message="ページ移動中..." />
+        </div>
+      )}
+    </>
+  );
 }
