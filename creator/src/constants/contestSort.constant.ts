31fb0876974b7321001a3a4f448034a70b25c403
kyatomaru <souryu1010@gmail.com>
2025-10-29 14:07:11 +0900

    feat: フィルターの選択肢をオブジェクトで定義

---
+++ creator/src/constants/contestSort.constant.ts
@@ -0,0 +1,14 @@
+export const CONTEST_FILTER_OPTIONS = [
+  {
+    label: "全て",
+    value: "all",
+  },
+  {
+    label: "開催中",
+    value: "ongoing",
+  },
+  {
+    label: "終了",
+    value: "ended",
+  },
+];
