f37fdf9600141c1bfed8429ceebbc43b0363a8c9
kyatomaru <souryu1010@gmail.com>
2025-11-08 15:24:35 +0900

    feat: 公式LINE登録ボタンの追加

--- creator/src/components/registerLineSection.tsx
+++ creator/src/components/registerLineSection.tsx
@@ -1,40 +1,29 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function RegisterLineSection() {
-  useEffect(() => {
-    // LINEのスクリプトを動的に読み込み
-    const script = document.createElement("script");
-    script.src =
-      "https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // クリーンアップ時にスクリプトを削除
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div>
-      {/* <div className="flex flex-col items-center justify-center"> */}
-      <h2 className="text-sm font-bold text-muted-foreground px-2 py-2">
-        公式LINEを登録して最新のコンテスト情報をうけとりましょう
-      </h2>
-      <div
-        className="line-it-button"
-        data-lang="ja"
-        data-type="friend"
-        //  data-size="large"
-        data-env="PROD"
-        data-lineid="@835angbx"
-        style={{ display: "none" }}
-      ></div>
-      {/* <a href="https://lin.ee/1UN92jO"><Image src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="25" width="110" style={{ border: 0, height: "35px" }} /></a> */}
+    <div className="flex flex-col gap-2 items-start justify-between pb-4">
+      <p className="text-sm">最新のコンテスト情報を受け取りましょう</p>
+      <Link
+        href="https://lin.ee/hPx39td"
+        target="_blank"
+        className="flex items-center gap-2"
+      >
+        <Image
+          src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"
+          alt="友だち追加"
+          height="30"
+          width="110"
+        />
+      </Link>
     </div>
   );
 }
