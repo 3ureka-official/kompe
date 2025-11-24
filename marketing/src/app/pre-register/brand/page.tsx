dda6bcded20269ada039861a2df6848b97735b47
kyatomaru <souryu1010@gmail.com>
2025-11-24 15:40:14 +0900

    refactor: 不要なファイル削除

--- marketing/src/app/pre-register/brand/page.tsx
+++
@@ -1,5 +0,0 @@
-import { BrandPreRegisterForm } from "@/components/pre-register/BrandPreRegisterForm";
-
-export default function BrandPreRegisterPage() {
-  return <BrandPreRegisterForm />;
-}
--- marketing/src/app/pre-register/creator/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CreatorPreRegisterForm } from "@/components/pre-register/CreatorPreRegisterForm";
-
-export default function CreatorPreRegisterPage() {
-  return <CreatorPreRegisterForm />;
-}
