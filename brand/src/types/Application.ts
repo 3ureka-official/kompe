8c1677cf34714e6ea7b2a4496171d01f18e176f5
kyatomaru <souryu1010@gmail.com>
2025-12-03 11:45:03 +0900

    refactor: update Application type imports

--- brand/src/types/Application.ts
+++ brand/src/types/Application.ts
@@ -1,5 +1,6 @@
 import { Creator } from "./Creator";
 import { ContestTransfer } from "./ContestTransfer";
+import { PurchaseProofStatus } from "./PurchaseProofStatus";
 
 /**
  * アプリケーション関連の型定義
@@ -16,4 +17,7 @@ export type Application = {
   created_at: Date;
   creator: Creator;
   contest_transfer: ContestTransfer | null;
+  purchase_proof_image_url?: string | null;
+  purchase_proof_status?: PurchaseProofStatus;
+  purchase_proof_rejection_comment?: string | null;
 };
