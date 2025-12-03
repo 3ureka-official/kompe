00aa3b0cd205e8c0bd3e54df99cc8e5fa90b93f5
kyatomaru <souryu1010@gmail.com>
2025-12-03 11:43:30 +0900

    feat: add login form component

---
+++ brand/src/features/auth/login/components/ui-parts/LoginForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import { UseFormHandleSubmit, Control } from "react-hook-form";
+import { LoginFormData } from "@/features/auth/login/schemas/loginUserSchema";
+import { FormInputField } from "@/features/auth/common/components/ui-elements/FormInputField";
+import { FormErrorMessage } from "@/features/auth/common/components/ui-elements/FormErrorMessage";
+import { SubmitButton } from "@/features/auth/common/components/ui-elements/SubmitButton";
+import { AuthFormLink } from "@/features/auth/common/components/ui-elements/AuthFormLink";
+
+interface LoginFormProps {
+  control: Control<LoginFormData>;
+  handleSubmit: UseFormHandleSubmit<LoginFormData>;
+  onSubmit: () => void;
+  isPending: boolean;
+  error: Error | null;
+  isAuthLoading: boolean;
+}
+
+export function LoginForm({
+  control,
+  handleSubmit,
+  onSubmit,
+  isPending,
+  error,
+  isAuthLoading,
+}: LoginFormProps) {
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-6">
+        <FormInputField control={control} name="email" type="email" required />
+        <FormInputField
+          control={control}
+          name="password"
+          type="password"
+          required
+        />
+      </div>
+
+      <div>
+        <FormErrorMessage message={error?.message} />
+
+        <SubmitButton
+          isLoading={isPending || isAuthLoading}
+          loadingText="ログイン中..."
+          defaultText="ログイン"
+        />
+      </div>
+      <AuthFormLink href="/auth/signup" text="アカウントを作成" />
+    </form>
+  );
+}
